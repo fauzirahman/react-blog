3e318e7cfc5d35121875f4b393a81f1ba5a00e66
fauzi <fauzirahman45@gmail.com>
2020-02-20 17:41:32 +0700

    app build

---
+++ build/precache-manifest.feca79fd71594f1aaa872373a57f5b9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfc40b2091a5ab7c41ad32159810f3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97264e806d1ad46cdfd2",
+    "url": "/static/js/2.301be125.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.301be125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18fab6f9f2b910447fe",
+    "url": "/static/js/main.338990f5.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
